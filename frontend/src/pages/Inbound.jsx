ff153a3be7501892bdb38c6a77c2dc3b8645a984
FIGARO79 <ff19790405@gmail.com>
2026-04-07 02:22:25 +0000

    style: adjust container height and form margin in Inbound page layout

--- frontend/src/pages/Inbound.jsx
+++ frontend/src/pages/Inbound.jsx
@@ -643,8 +643,8 @@ const Inbound = () => {
 
     return (
         <>
-            <div className="container-wrapper px-4 pt-2 pb-4 lg:h-[calc(100vh-30px)] lg:flex lg:flex-col lg:overflow-hidden">
-                <form onSubmit={handleSaveLog} className="lg:flex-shrink-0 mb-4">
+            <div className="container-wrapper px-4 pt-2 pb-4 lg:h-[calc(100vh-10px)] lg:flex lg:flex-col lg:overflow-hidden">
+                <form onSubmit={handleSaveLog} className="lg:flex-shrink-0 mb-1">
 
                     <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-2">
                         <div className="lg:col-span-2 bg-white p-2 rounded shadow border border-gray-200">
